b187b58d3f61fdf280b5706a12d2a077e8545a1d
tomaskurten <tomas.kurten@bue.edu.ar>
2023-09-25 20:01:24 -0300

    primero

---
+++ JS1/js/script.js
@@ -0,0 +1,67 @@
+console.log("Hola Mundo!")
+console.log("Hola Mundo!")
+console.log("Hola Mundo!")
+console.log("Hola Mundo!")
+// alert("Ventana de alerta!")
+// document.write("Texto desde JS")
+// document.write("<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quos repudiandae atque corrupti animi fuga perferendis reprehenderit alias sapiente error. Repellendus cum similique iure. Veniam, deleniti! Quisquam saepe facilis possimus!</p>")
+
+
+// //tipado dinamico
+// s = "Hola, me llamo Juan y tengo 100 años" // s, de string
+// console.log(typeof s)
+// s = 28 // n, de número
+// console.log(typeof s)
+// s = true // b, de booleano
+// console.log(typeof s)
+// s = "ahora" // s, de string
+// console.log(typeof s)
+
+
+// nombre=prompt("Ingrese su nombre")
+// console.log("Mi nombre es: ",nombre)
+// document.write("Mi nombre es: ",nombre)
+
+
+// console.log("Mi edad es: ",edad)
+// edad=parseInt(prompt("Ingrese su edad"))
+// document.write("Mi nombre es: ",nombre," y mi edad es: "+edad)
+// console.log(typeof nombre)
+
+
+// console.log(typeof edad)
+// console.log(edad+10)
+
+// console.clear()
+// numero=10
+// console.log(Number.isFinite(numero))
+// numero=10
+// respuesta=Number.isInteger(numero)
+// console.log(respuesta)
+
+
+// Number.parseInt("42"); // 42
+// Number.parseInt("42€"); // 42
+// Number.parseInt("Núm. 42"); // NaN
+// Number.parseInt("A");
+
+console.clear()
+// txt1 = "Juan"
+// txt2 = "Pablo"
+// txt3 = txt1 + "  " + txt2
+// console.log(txt3)
+
+// txt1 = 3
+// txt2 = 5
+// txt3 = txt1 ** txt2
+// console.log(txt3)
+
+
+i=0
+i++
+console.log(i)
+i++
+console.log(i)
+i=i+1
+console.log(i)
+
