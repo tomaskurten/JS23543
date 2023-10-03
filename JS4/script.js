8ab718582f5548f405b959f8157aff23d2be7775
tomaskurten <tomas.kurten@bue.edu.ar>
2023-10-03 00:43:56 -0300

    JS4 agregada

---
+++ JS4/script.js
@@ -0,0 +1,92 @@
+// nombre="Ron"
+// edad=2
+// color="marron"
+
+// var Perro={
+//     nombre:"Ron",
+//     edad:2,
+//     color: "marron",
+//     ladrar: function(){
+//         console.log("Guau!")
+//     },
+//     saltar: function(){
+//         return "El perró saltó"
+//     }
+// }
+
+// console.log(Perro)
+// console.clear()
+// console.log(Perro.color)
+// console.log(Perro.edad)
+// console.log(Perro.nombre)
+// console.log(Perro.ladrar())
+// salto=Perro.saltar()
+// console.log(salto)
+
+
+// class Perro{
+//     constructor(nombre,edad,color){
+//         this.nombre=nombre
+//         this.edad=edad
+//         this.color=color
+//         this.ladrar=function(){
+//             console.log(this.nombre," dice: Guau!")
+//         }
+//     }
+// }
+
+// var perro1= new Perro("Ron",2,"Marrón")
+// var perro2= new Perro("Luna",9,"Negro")
+
+// console.log("Perro: ", perro1.nombre, " Color: ",perro1.color," Edad: ",perro1.edad)
+// console.log("Perro: ", perro2.nombre, " Color: ",perro2.color," Edad: ",perro2.edad)
+// console.log(perro1.ladrar())
+// console.log(perro2.ladrar())
+
+// class Vehiculo{
+//     constructor(marca,modelo,color){
+//         this.marca=marca
+//         this.modelo=modelo
+//         this.color=color
+//     }
+// }
+
+
+// function Auto(marca, tipo, modelo) {
+//     this.marca = marca
+//     this.tipo = tipo
+//     this.modelo = modelo
+//     }
+
+// //Creamos el objeto miAuto
+// var miAuto = new Auto('Ford','Focus', 2019)
+// //Creamos el objeto miFurgon
+// var miFurgon = new Auto('Renault','Traffic', 2010)
+
+// console.log(miAuto.marca)
+// console.log(miFurgon.modelo)
+   
+// var texto1="Hola mundo!"
+// texto3=", Adios!"
+// console.log(texto1.length)
+// texto2=texto1.concat(texto3)
+
+// console.log(texto2)
+
+// var cad="Holaaaa"
+// // console.log(cad.indexOf("a")) 
+// // console.log(cad.lastIndexOf("a"))
+
+// console.log(cad.toUpperCase())
+// var nombre = "Juan"
+// edad=20
+// console.log(`¡Hola ${nombre}, edad: ${edad}`)
+// console.log("Hola ",nombre,"!")
+// console.log("Hola "+nombre+"!")
+// var cadena = `Línea número 1 de la cadena
+// Línea número 2 de la cadena`
+// console.log(cadena)
+
+// console.log(Math.PI)
+// console.log(Math.cos(0))
+// console.log(Math.exp(3))
